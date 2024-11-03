16b242934d8a939ad49b13c05860e16c88584f6d
あなたの名前 <kametyanhaku@gmail.com>
2024-11-03 22:40:01 +0900

    初回コミット

--- src/test.py
+++ src/test.py
@@ -58,4 +58,4 @@ async def search_tweets(request: SearchRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
